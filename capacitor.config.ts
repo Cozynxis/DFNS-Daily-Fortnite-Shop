ab2dc4ff2bbf4bf464bf328a172b4e4a1f49fcec
Cozynxis/Levi <levi.jvbcollege@gmail.com>
2026-08-20 12:31:58 +0200

    chore: add Capacitor configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dfns.dailyfortniteshop',
+  appName: 'DFNS',
+  webDir: 'www'
+};
+
+export default config;
