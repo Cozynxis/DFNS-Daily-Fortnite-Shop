5bc2216abc01318e3724f7f5b9e9f94bcee99ec6
Cozynxis/Levi <levi.jvbcollege@gmail.com>
2026-08-21 15:02:52 +0200

    Add DFNS reaction backend configuration

---
+++ js/reactions-config.js
@@ -0,0 +1,9 @@
+/* DFNS reaction backend configuration.
+ *
+ * Create a free Supabase project, then put the Project URL and the anon/public key
+ * below. The anon key is intended for browser use; NEVER put a service_role key here.
+ */
+window.DFNS_REACTIONS_CONFIG = {
+  url: "",
+  anonKey: ""
+};
